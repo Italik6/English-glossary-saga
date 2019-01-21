79c564c42f39e71b5617423eec23b7f524229ad2
Michal Turek <michael.turek@rizk.com>
2019-01-21 13:14:10 +0100

    redux-saga-init

---
+++ src/containers/SearchBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.setState({ term: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Search a word"
+          value={this.state.term}
+          className="form-control"
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Search
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
