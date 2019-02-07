c7594afa016ae2d44a759dbb35e64a7398e3f725
Michal Turek <michael.turek@rizk.com>
2019-02-07 16:56:13 +0100

    add-interactive-list-for-rhymes

--- src/containers/WordData.js
+++ src/containers/WordData.js
@@ -3,7 +3,9 @@ import Alert from "../components/alert";
 import Button from "../components/button";
 import List from "../components/list";
 import Loader from "../components/loader";
+import InteractiveList from "../components/interactiveList";
 import { searchRhymesAction } from "../actions/SearchRhymes";
+import { searchWordAction } from "../actions/SearchWord";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
@@ -13,18 +15,34 @@ class WordData extends Component {
     this.props.searchRhymesAction(this.props.wordData.word);
   };
 
+  onClickItem = item => {
+    this.props.searchWordAction(item);
+  };
+
   definitionsComponent = () => {
     const { wordData } = this.props;
     if (wordData.results) {
       const items = wordData.results.map(a => a.definition);
-      return <List title={"Definitions"} items={items} />;
+      return (
+        <List
+          title={"Definitions"}
+          items={items}
+          onClick={item => this.onClickItem(item)}
+        />
+      );
     }
   };
 
   rhymesComponent = () => {
     const { rhymes } = this.props;
     if (rhymes.all) {
-      return <List title={"Rhymes"} items={rhymes.all} />;
+      return (
+        <InteractiveList
+          title={"Rhymes"}
+          items={rhymes.all}
+          onClick={item => this.onClickItem(item)}
+        />
+      );
     }
   };
 
@@ -65,7 +83,7 @@ function mapStateToProps({ wordData, isError, rhymes }) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ searchRhymesAction }, dispatch);
+  return bindActionCreators({ searchRhymesAction, searchWordAction }, dispatch);
 }
 
 export default connect(
